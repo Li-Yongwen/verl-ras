e6041db5834b244557a9d1dfe76ba4210dd94dca
zyang6 <zhouyang271@huawei.com>
2025-11-06 19:28:49 +0800

    .

--- tests/special_npu/run_qwen3_30b_dapo_mindspeed.sh
+++ tests/special_npu/run_qwen3_30b_dapo_mindspeed.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-MODEL_ID=Qwen3-30B-A3B-Instruct #!
+MODEL_ID=Qwen3-30B-A3B-Instruct
 MODEL_PATH=/mnt/hpfs_test/weights/Qwen3-30B-A3B-Instruct
 DUMMY_MODEL_PATH=/mnt/hpfs_test/weights/Qwen3-30B-A3B-Instruct-dummy
 USE_DIST_CKPT=True
 USE_DUMMY_MODEL=True
 DUMMY_MODEL_CONFIG_PATH=tests/special_e2e/ppo_trainer/expert_parallel/qwen3moe_minimal.json
 
-#############################################
+mkdir -p ${HOME}/data/dapo-math-17k
+\cp -f /mnt/hpfs_test/data/data/dapo-math-17k.parquet ${HOME}/data/dapo-math-17k/dapo-math-17k.parquet
 
-NUM_GPUS=${NUM_GPUS:-16}
+#############################################
 
 MODEL_ID=${MODEL_ID:-Qwen/Qwen3-30B-Instruct}
 MODEL_PATH=${MODEL_PATH:-${HOME}/models/${MODEL_ID}}
@@ -23,6 +24,10 @@ if [ "$USE_DUMMY_MODEL" = "True" ]; then
         echo "[ERROR] DUMMY_MODEL_CONFIG_PATH not set"
         exit 1
     fi
+    if [[ -d $DUMMY_MODEL_PATH && $DUMMY_MODEL_PATH != "/" ]]; then
+        rm -rf $DUMMY_MODEL_PATH
+    fi
+
     python scripts/init_random_model.py \
         --hf_model_path "${MODEL_PATH}" \
         --new_config_path "${DUMMY_MODEL_CONFIG_PATH}" \
@@ -36,36 +41,11 @@ if [ "$USE_DIST_CKPT" = "True" ]; then
         --output_path "${DIST_CKPT_PATH}"
 fi
 
-exit 0
-
-
-
-if [[ ${USE_DIST_CKPT} == "True" ]]; then
-    if [[ ${USE_DUMMY_MODEL} == "True" ]]; then
-        DIST_CKPT_PATH=${HOME}/dist_ckpt_dummy/${MODEL_ID}
-    fi
-    python scripts/converter_hf_to_mcore.py \
-        --hf_model_path ${MODEL_PATH} \
-        --output_path ${DIST_CKPT_PATH}
-fi
 
 exp_name='CI-DAPO-Qwen3-30B-megatron'
 
 # Megatron backen
-train_tp=2
-train_ep=2
-train_pp=2
-train_cp=1
 
-adv_estimator=grpo
-
-kl_coef=0.0
-use_kl_in_reward=False
-use_kl_loss=False
-kl_loss_coef=0.0
-
-clip_ratio_low=0.2
-clip_ratio_high=0.28
 
 max_prompt_length=1024
 max_response_length=2048
@@ -73,136 +53,85 @@ enable_overlong_buffer=True
 overlong_buffer_len=128
 overlong_penalty_factor=1.0
 
-loss_agg_mode="token-mean"
-
-enable_filter_groups=False # True may cause CI Failded, since answer be cut by max_response_length
-filter_groups_metric=acc
-max_num_gen_batches=10
 train_prompt_bsz=16
-gen_prompt_bsz=$((train_prompt_bsz * 2))
-n_resp_per_prompt=4
-train_prompt_mini_bsz=2
 
-# Paths
-MODEL_PATH=${MODEL_PATH:-"${RAY_DATA_HOME}/models/Qwen3-30B-A3B"}
-
-MCORE_MODEL_PATH=${MCORE_MODEL_PATH:-"${RAY_DATA_HOME}/models/Qwen3-30B-A3B-dist_ckpt"}
-CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
-
-# Algorithm
-temperature=1.0
-top_p=1.0
-top_k=-1 # 0 for HF rollout, -1 for vLLM rollout
-
-# Performance Related Parameter
-sp_size=8
-use_dynamic_bsz=True
 actor_ppo_max_token_len=$(((max_prompt_length + max_response_length)))
 infer_ppo_max_token_len=$(((max_prompt_length + max_response_length)))
-offload=True
-
-max_num_batched_tokens=$((max_prompt_length + max_response_length))
-
-
 
 python3 -m recipe.dapo.main_dapo \
     --config-name="dapo_megatron_trainer" \
+    data.train_files=${HOME}/data/dapo-math-17k/dapo-math-17k.parquet \
+    data.val_files=${HOME}/data/dapo-math-17k/dapo-math-17k.parquet \
+    data.train_batch_size=${train_prompt_bsz} \
+    data.max_prompt_length=${max_prompt_length} \
+    data.max_response_length=${max_response_length} \
     data.filter_overlong_prompts=False \
-    data.train_files=${HOME}/data/gsm8k/train.parquet \
-    data.val_files="${HOME}/data/gsm8k/test.parquet" \
     data.shuffle=False \
     data.prompt_key=prompt \
     data.truncation='left' \
-    data.max_prompt_length=${max_prompt_length} \
-    data.max_response_length=${max_response_length} \
-    data.gen_batch_size=${gen_prompt_bsz} \
-    data.train_batch_size=${train_prompt_bsz} \
-    algorithm.adv_estimator=${adv_estimator} \
-    algorithm.use_kl_in_reward=${use_kl_in_reward} \
-    algorithm.kl_ctrl.kl_coef=${kl_coef} \
-    actor_rollout_ref.rollout.n=${n_resp_per_prompt} \
-    actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.actor.use_kl_loss=${use_kl_loss} \
-    actor_rollout_ref.actor.kl_loss_coef=${kl_loss_coef} \
-    actor_rollout_ref.actor.clip_ratio_low=${clip_ratio_low} \
-    actor_rollout_ref.actor.clip_ratio_high=${clip_ratio_high} \
+    data.gen_batch_size=$((train_prompt_bsz * 2)) \
+    algorithm.adv_estimator=grpo \
+    algorithm.use_kl_in_reward=False \
+    actor_rollout_ref.actor.kl_loss_coef=0.0 \
+    actor_rollout_ref.actor.clip_ratio_low=0.2 \
+    actor_rollout_ref.actor.clip_ratio_high=0.28 \
     actor_rollout_ref.actor.clip_ratio_c=10.0 \
     actor_rollout_ref.actor.ppo_epochs=1 \
-    algorithm.filter_groups.enable=${enable_filter_groups} \
-    algorithm.filter_groups.max_num_gen_batches=${max_num_gen_batches} \
-    algorithm.filter_groups.metric=${filter_groups_metric} \
-    actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz} \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.ref.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
-    actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=${use_dynamic_bsz} \
+    algorithm.filter_groups.enable=False \
+    algorithm.filter_groups.max_num_gen_batches=10 \
+    algorithm.filter_groups.metric=acc \
+    actor_rollout_ref.actor.use_dynamic_bsz=True \
+    actor_rollout_ref.model.path="${MODEL_PATH}" \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps=10 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=20 \
+    actor_rollout_ref.actor.strategy=megatron \
+    actor_rollout_ref.ref.log_prob_use_dynamic_bsz=True \
+    actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${actor_ppo_max_token_len} \
     actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
-    actor_rollout_ref.model.path="${MODEL_PATH}" \
-    +actor_rollout_ref.model.override_config.attention_dropout=0. \
-    +actor_rollout_ref.model.override_config.embd_pdrop=0. \
-    +actor_rollout_ref.model.override_config.resid_pdrop=0. \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
-    +actor_rollout_ref.critic.optim.lr=5e-8 \
-    actor_rollout_ref.actor.optim.lr_warmup_steps=10 \
-    actor_rollout_ref.actor.ppo_mini_batch_size=${train_prompt_mini_bsz} \
-    actor_rollout_ref.actor.megatron.param_offload=${offload} \
-    actor_rollout_ref.actor.megatron.optimizer_offload=${offload} \
-    actor_rollout_ref.actor.megatron.grad_offload=${offload} \
-    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=${train_pp} \
-    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=${train_tp} \
-    actor_rollout_ref.actor.megatron.expert_model_parallel_size=${train_ep} \
-    actor_rollout_ref.actor.megatron.context_parallel_size=${train_cp} \
-    actor_rollout_ref.actor.megatron.dist_checkpointing_path=${DIST_CKPT_PATH} \
+    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=2 \
+    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=2 \
+    actor_rollout_ref.actor.megatron.expert_model_parallel_size=2 \
     actor_rollout_ref.actor.megatron.use_dist_checkpointing=True \
-    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=${train_pp} \
-    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=${train_tp} \
-    actor_rollout_ref.ref.megatron.expert_model_parallel_size=${train_ep} \
-    actor_rollout_ref.ref.megatron.context_parallel_size=${train_cp} \
-    actor_rollout_ref.ref.megatron.param_offload=${offload} \
     actor_rollout_ref.actor.megatron.dist_checkpointing_path=${DIST_CKPT_PATH} \
-    actor_rollout_ref.actor.entropy_coeff=0 \
-    actor_rollout_ref.actor.loss_agg_mode=${loss_agg_mode} \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=${gen_tp} \
-    actor_rollout_ref.rollout.enable_chunked_prefill=True \
-    actor_rollout_ref.rollout.enable_prefix_caching=False \
-    actor_rollout_ref.rollout.max_num_batched_tokens=${max_num_batched_tokens} \
-    actor_rollout_ref.rollout.max_model_len=$((max_prompt_length + max_response_length)) \
-    actor_rollout_ref.rollout.temperature=${temperature} \
-    actor_rollout_ref.rollout.top_p=${top_p} \
-    actor_rollout_ref.rollout.top_k=${top_k} \
-    actor_rollout_ref.rollout.val_kwargs.temperature=${temperature} \
-    actor_rollout_ref.rollout.val_kwargs.top_p=${top_p} \
-    actor_rollout_ref.rollout.val_kwargs.top_k=${top_k} \
-    actor_rollout_ref.rollout.val_kwargs.do_sample=True \
-    actor_rollout_ref.rollout.val_kwargs.n=1 \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.loss_agg_mode="token-mean" \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=40 \
+    actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.n=5 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=40 \
+    actor_rollout_ref.ref.strategy=megatron \
+    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=2 \
+    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=2 \
+    actor_rollout_ref.ref.megatron.expert_model_parallel_size=2 \
+    actor_rollout_ref.rollout.temperature=1.0 \
+    actor_rollout_ref.rollout.top_p=1.0 \
+    actor_rollout_ref.rollout.top_k=-1 \
     actor_rollout_ref.rollout.enforce_eager=True \
     actor_rollout_ref.rollout.free_cache_engine=True \
     actor_rollout_ref.ref.megatron.use_dist_checkpointing=True \
+    actor_rollout_ref.actor.megatron.dist_checkpointing_path=${DIST_CKPT_PATH} \
     reward_model.reward_manager=dapo \
     reward_model.overlong_buffer.enable=${enable_overlong_buffer} \
     reward_model.overlong_buffer.len=${overlong_buffer_len} \
     reward_model.overlong_buffer.penalty_factor=${overlong_penalty_factor} \
+    algorithm.kl_ctrl.kl_coef=0.0 \
     trainer.logger=['console'] \
-    trainer.project_name='verl-test' \
-    trainer.experiment_name="${exp_name}" \
-    trainer.n_gpus_per_node=${NUM_GPUS} \
+    trainer.project_name='verl_dapo_example_mathdapo17k' \
+    trainer.experiment_name='qwen3_30b_cut_dapo_mindspeed' \
+    trainer.n_gpus_per_node=16 \
     trainer.nnodes=1 \
-    trainer.val_before_train=False \
-    trainer.test_freq=-1 \
     trainer.save_freq=-1 \
+    trainer.test_freq=-1 \
     trainer.total_epochs=1 \
-    trainer.total_training_steps=1 \
-    trainer.default_local_dir="${CKPTS_DIR}" \
-    trainer.device="npu" \
-    actor_rollout_ref.nccl_timeout=14400 \
+    trainer.total_training_steps=2 \
+    trainer.device=npu \
     actor_rollout_ref.actor.use_torch_compile=False \
     actor_rollout_ref.ref.use_torch_compile=False \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.use_flash_attn=True \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_method=uniform \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_granularity=full \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_num_layers=1 $@
+    +actor_rollout_ref.actor.megatron.override_transformer_config.use_flash_attn=True $@
