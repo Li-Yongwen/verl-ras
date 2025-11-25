c1d506561eb46a55f737b5c3707db1f7a3151b9f
liyongwen <1310439159@qq.com>
2025-11-25 17:25:10 +0800

    【colocated】get_gen_batch修改回退

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -535,21 +535,11 @@ class RayPPOTrainer:
         # Log to each configured logger
         self.validation_generations_logger.log(self.config.trainer.logger, samples, self.global_steps)
 
-    def _get_gen_batch(
-        self,
-        batch: DataProto,
-        input_batch_keys_to_pop: List[str] = [
-            "input_ids",
-            "attention_mask",
-            "position_ids",
-        ],
-    ) -> DataProto:
-        reward_model_keys = (
-            set({"data_source", "reward_model", "extra_info"})
-            & batch.non_tensor_batch.keys()
-        )
+    def _get_gen_batch(self, batch: DataProto) -> DataProto:
+        reward_model_keys = set({"data_source", "reward_model", "extra_info", "uid"}) & batch.non_tensor_batch.keys()
+
         # pop those keys for generation
-        batch_keys_to_pop = input_batch_keys_to_pop
+        batch_keys_to_pop = ["input_ids", "attention_mask", "position_ids"]
         non_tensor_batch_keys_to_pop = set(batch.non_tensor_batch.keys()) - reward_model_keys
         gen_batch = batch.pop(
             batch_keys=batch_keys_to_pop,
